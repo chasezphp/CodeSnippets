a607cf0bf0b8e191815214541c805bbc6d45cbfd
Allen <liuxidong1984@gmail.com>
2017-01-13 10:18:29 +0800

    add:ksort.js

---
+++ JavaScript/ksort.js
@@ -0,0 +1,9 @@
+//对象按照key排序
+function ksort(obj) {
+    var newkey = Object.keys(obj).sort();
+    var newObj = {};
+    for (var i = 0; i < newkey.length; i++) {
+        newObj[newkey[i]] = obj[newkey[i]];
+    }
+    return newObj;
+}
